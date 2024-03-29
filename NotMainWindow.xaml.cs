25acdab9a657ae6a760d97828e8a397cb66d8c30
dashamamasalieva <daramamasalieva39067@gmail.com>
2024-03-29 13:02:06 +0300

    Добавьте файлы проекта.

---
+++ NotMainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace begunBezikov
+{
+    /// <summary>
+    /// Логика взаимодействия для NotMainWindow.xaml
+    /// </summary>
+    public partial class NotMainWindow : Window
+    {
+        public NotMainWindow()
+        {
+            InitializeComponent();
+            Frame MainFR = new Frame();
+            MainFR.Navigate(new RegAsRun());
+        }
+    }
+}
---
+++ hlp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace begunBezikov
+{
+    internal class hlp
+    {
+        public static Frame frame;
+    }
+}
